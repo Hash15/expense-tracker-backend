b98f3e2f68eed310ba7adcba87895d904b258a0c
Hash15 <koustubhwale85@gmail.com>
2025-11-19 15:26:04 +0530

    Added deployed frontend origin

--- src/main/java/com/example/expensetracker/config/AppConfig.java
+++ src/main/java/com/example/expensetracker/config/AppConfig.java
@@ -23,7 +23,7 @@ public class AppConfig {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
 
-    config.setAllowedOrigins(List.of("http://localhost:5173"));
+    config.setAllowedOrigins(List.of("http://localhost:5173","https://expensetracker-frontend-roan.vercel.app/"));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     config.setAllowedHeaders(List.of("*"));
     config.setAllowCredentials(true);
